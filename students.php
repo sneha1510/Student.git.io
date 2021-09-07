a42028d7a469735ffd77412617db90c0ffb863a1
sneha1510 <66209707+sneha1510@users.noreply.github.com>
2021-09-07 13:40:54 +0530

    Add files via upload

---
+++ students.php
@@ -0,0 +1,51 @@
+<html>
+   <form action="Result.php">
+      <table>
+         <caption>STUDENT MARKSHEET FORM</caption>
+         <tr>
+            <td>School Name</td>
+            <td><input type=text name=sn size=30></td>
+         </tr>
+         <tr>
+            <td>Student Name</td>
+            <td><input type=text name=stn size=30></td>
+         </tr>
+         <tr>
+            <td>Father's Name</td>
+            <td><input type=text name=fn size=30></td>
+         </tr>
+         <tr>
+            <td>Dob</td>
+            <td><input type=text name=dob size=30></td>
+         </tr>
+         <tr>
+            <td>Gender</td>
+            <td><input type=radio name=gen size=30 value="Male">Male<input type=radio name=gen size=30 value="Female">Female</td>
+         </tr>
+         <tr>
+            <td>Hindi marks</td>
+            <td><input type=text name=hin size=30></td>
+         </tr>
+         <tr>
+            <td>English marks</td>
+            <td><input type=text name=eng size=30></td>
+         </tr>
+         <tr>
+            <td>Maths marks</td>
+            <td><input type=text name=math size=30></td>
+         </tr>
+         <tr>
+            <td>Physics marks</td>
+            <td><input type=text name=phy size=30></td>
+         </tr>
+         <tr>
+            <td>Chemistry marks</td>
+            <td><input type=text name=chem size=30></td>
+         </tr>
+         <tr>
+            <td><input type=submit></td>
+            <td><input type=reset></td>
+         </tr>
+      </table>
+   </form>
+</html>
